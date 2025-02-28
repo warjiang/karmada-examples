c6823190f382980610ca1a60c2da6f42021c2404
warjiang <1096409085@qq.com>
2025-02-28 11:51:29 +0800

    feat: add attach-container
    Signed-off-by: warjiang <1096409085@qq.com>

---
+++ attach-container/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/karmada-io/dashboard/pkg/client"
+	"github.com/karmada-io/dashboard/pkg/environment"
+	"net/http"
+	"os"
+)
+
+func main() {
+	kubeconfigPath := os.Getenv("KUBECONFIG_PATH")
+	kubeContext := os.Getenv("KUBE_CONTEXT")
+	skipTLSVerify := os.Getenv("SKIP_TLS_VERIFY") == "true"
+	serverAddress := os.Getenv("SERVER_ADDRESS")
+
+	client.InitKubeConfig(
+		client.WithUserAgent(environment.UserAgent()),
+		client.WithKubeconfig(kubeconfigPath),
+		client.WithKubeContext(kubeContext),
+		client.WithInsecureTLSSkipVerify(skipTLSVerify),
+	)
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pang")
+	})
+	r.GET("/pod/:namespace/:pod/shell/:container", handleExecShell)
+
+	http.Handle("/", r)
+	http.Handle("/api/sockjs/", CreateAttachHandler("/api/sockjs"))
+
+	err := http.ListenAndServe(serverAddress, nil)
+	if err != nil {
+		fmt.Printf("Server failed to start. Error: %v\n", err)
+	}
+}
