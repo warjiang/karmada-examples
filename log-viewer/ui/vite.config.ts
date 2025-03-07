9139a6dc882959f5317fe58a54f788800646d5b4
warjiang <1096409085@qq.com>
2025-03-08 01:13:32 +0800

    feat: init ui for log-viewer
    Signed-off-by: warjiang <1096409085@qq.com>

---
+++ log-viewer/ui/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  server: {
+    proxy: {
+      '^/api/': {
+        target: 'http://localhost:9000',
+        changeOrigin: true,
+        headers: {
+          // cookie: env.VITE_COOKIES,
+          // Authorization: `Bearer ${env.VITE_TOKEN}`
+        },
+      },
+      '^/api/v1/misc/sockjs*': {
+        target: 'ws://localhost:9000',
+        changeOrigin: false,
+        secure: false,
+        ws: true,
+      },
+    }
+  }
+})
