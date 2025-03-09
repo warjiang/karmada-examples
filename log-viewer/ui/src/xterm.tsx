909dc126b1bf51401ccec0ecb1c7c4ae818a67c8
warjiang <1096409085@qq.com>
2025-03-09 15:20:13 +0800

    feat: add options for xterm.tsx
    Signed-off-by: warjiang <1096409085@qq.com>

--- log-viewer/ui/src/xterm.tsx
+++ log-viewer/ui/src/xterm.tsx
@@ -1,4 +1,4 @@
-import { type ITerminalAddon, type ITerminalInitOnlyOptions, type ITerminalOptions, Terminal } from '@xterm/xterm'
+import {type ITerminalAddon, type ITerminalInitOnlyOptions, type ITerminalOptions, ITheme, Terminal} from '@xterm/xterm'
 import '@xterm/xterm/css/xterm.css'
 import { type ComponentPropsWithoutRef, useEffect, useRef, useState } from 'react'
 
@@ -28,11 +28,33 @@ export function useXTerm({ options, addons, listeners }: UseXTermProps = {}) {
         const instance = new Terminal({
             fontFamily: 'operator mono,SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace',
             fontSize: 14,
+            // theme: {
+            //     background: '#101420',
+            // },
+            // cursorStyle: 'underline',
+            // cursorBlink: false,
+            convertEol: true,
             theme: {
-                background: '#101420',
-            },
-            cursorStyle: 'underline',
-            cursorBlink: false,
+                foreground: '#d2d2d2',
+                background: '#2b2b2b',
+                cursor: '#adadad',
+                black: '#000000',
+                red: '#d81e00',
+                green: '#5ea702',
+                yellow: '#cfae00',
+                blue: '#427ab3',
+                magenta: '#89658e',
+                cyan: '#00a7aa',
+                white: '#dbded8',
+                brightBlack: '#686a66',
+                brightRed: '#f54235',
+                brightGreen: '#99e343',
+                brightYellow: '#fdeb61',
+                brightBlue: '#84b0d8',
+                brightMagenta: '#bc94b7',
+                brightCyan: '#37e6e8',
+                brightWhite: '#f1f1f0',
+            } as ITheme,
             ...options,
         })
 
