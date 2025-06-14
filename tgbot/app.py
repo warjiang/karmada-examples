2faf0588823731177094bec41b0cd17c9b8f288b
warjiang <1096409085@qq.com>
2025-06-15 00:44:22 +0800

    fix: add argv check in app
    Signed-off-by: warjiang <1096409085@qq.com>

--- tgbot/app.py
+++ tgbot/app.py
@@ -3,15 +3,25 @@ from telegram.ext import ApplicationBuilder
 import asyncio
 import os
 from datetime import datetime
+import sys
 
 async def main():
+    args = sys.argv
+    for arg in args:
+        print(arg)
+    if len(args) < 2:
+        print('Usage: uv run app.py <msg>')
+        exit(1)
+
+    msg = sys.argv[1]
+
     load_dotenv()
     bot_token = os.environ.get('BOT_TOKEN')
     chat_id = os.environ.get('CHAT_ID')
     application = ApplicationBuilder().token(bot_token).build()
     current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    msg = f'{current_date} image sync finished'
-    await application.bot.send_message(chat_id, msg)
+    msg_with_time = f'{current_date} {msg}'
+    await application.bot.send_message(chat_id, msg_with_time)
 
 
 if __name__ == '__main__':
