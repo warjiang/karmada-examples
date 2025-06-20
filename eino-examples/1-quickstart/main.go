5d25afbb7bf00570ad824163ff55c1d308134ca7
warjiang <1096409085@qq.com>
2025-06-21 00:53:52 +0800

    wip: add eino demo v1
    Signed-off-by: warjiang <1096409085@qq.com>

--- eino-examples/1-quickstart/main.go
+++ eino-examples/1-quickstart/main.go
@@ -1,11 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/cloudwego/eino-ext/components/model/openai"
+	"github.com/cloudwego/eino/schema"
+	"github.com/joho/godotenv"
+	"os"
+)
 
 func main() {
-	model, _ := openai.NewChatModel(ctx, config) // create an invokable LLM instance
-	message, _ := model.Generate(ctx, []*Message{
-		SystemMessage("you are a helpful assistant."),
-		UserMessage("what does the future AI App look like?")})
+	err := godotenv.Load()
+	if err != nil {
 
+	}
+	ctx := context.TODO()
+	config := &openai.ChatModelConfig{
+		BaseURL: os.Getenv("BASEURL"),
+		Model:   os.Getenv("MODEL"),
+		APIKey:  os.Getenv("APIKEY"),
+	}
+	model, _ := openai.NewChatModel(ctx, config) // create an invokable LLM instance
+	message, _ := model.Generate(ctx, []*schema.Message{
+		schema.SystemMessage("you are a helpful assistant."),
+		schema.UserMessage("what does the future AI App look like?"),
+	})
+	fmt.Println(message.Content)
 }
